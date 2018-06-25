2eb4471cd86a980569ea5d4dccc8b579d345b923
natgonzalezrosa <natgonzalezrosa@gmail.com>
2018-06-24 22:19:47 -0400

    added train logic to firebase watcher

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -15,56 +15,72 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 // Capture Button Click
-$("#add-user").on("click", function (event) {
+$("#add-train").on("click", function (event) {
 
     event.preventDefault();
 
     // Grabbed values from text boxes
-    var empName = $("#name-input").val().trim();
-    var empRole = $("#role-input").val().trim();
-    var empStart = moment($("#startdate-input").val().trim(), "DD/MM/YY").format("X");
-    var empRate = $("#monthlyrate-input").val().trim();
+    var trainName = $("#name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var trainTime = moment($("#traintime-input").val().trim(), "HH:mm").format("HH:mm");
+    var trainFrequency = $("#frequency-input").val().trim();
 
     // Code for handling the push
     database.ref().push( {
-        name: empName,
-        role: empRole,
-        start: empStart,
-        rate: empRate
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency
     });
 
     // Logs everything to console
-    console.log(empName);
-    console.log(empRole);
-    console.log(empStart);
-    console.log(empRate);
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(trainTime);
+    console.log(trainFrequency);
+    console.log("------------------");
 
     // Alert
-    alert("Employee successfully added!");
+    alert("Train successfully added!");
 
     $("#name-input").val("");
-    $("#role-input").val("");
-    $("#startdate-input").val("");
-    $("#monthlyrate-input").val("");
+    $("#destination-input").val("");
+    $("#traintime-input").val("");
+    $("#frequency-input").val("");
 });
 
 // Firebase watcher .on("child_added"
 database.ref().on("child_added", function(snapshot) {
     
     // Store everything into a variable
-    var empName = snapshot.val().name;
-    var empRole = snapshot.val().role;
-    var empStart = snapshot.val().start;
-    var empRate = snapshot.val().rate;
+    var trainName = snapshot.val().name;
+    var trainDestination = snapshot.val().destination;
+    var trainTime = snapshot.val().time;
+    var trainFrequency = snapshot.val().frequency;
 
-    // Convert the employee start
-    var empConvertedStart = moment.unix(empStart).format("MM/DD/YY");
+    // Convert the first train time
+    var firstTimeConverted = moment(trainTime, "HH:mm");
+    console.log("FIRST TRAIN TIME: " + moment(trainTime, "HH:mm").format("HH:mm"));
 
-    // Calculate the months worked
-    var empMonths = moment().diff(moment(empStart, "X"), "months");
-    
-    // Calculate the total billed rate
-    var empBilled = empMonths * empRate;
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
 
-    $("tbody").append("<tr><td>" + empName + "</td><td>" + empRole + "</td><td>" + empConvertedStart + "</td><td>" + empMonths + "</td><td>" + empRate + "</td><td>" + empBilled + "</td></tr>");
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var trainRemainder = diffTime % trainFrequency;
+    console.log(trainRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = trainFrequency - trainRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm A"));
+
+    $("tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + moment(nextTrain).format("hh:mm A") + "</td><td>" + tMinutesTillTrain + "</td><td></tr>");
 });
